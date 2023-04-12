0ed68befb3ef7b4ee76d6570556375bfceffc361
Victor Sousa <victorbrito001@gmail.com>
2023-04-12 17:48:02 -0300

    Inversão de dependências

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,37 +1,38 @@
 import { prisma } from '@/lib/prisma'
 import { hash } from 'bcryptjs'
 
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
-
 interface RegisterUseCaseRequest {
   name: string
   email: string
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
+export class RegisterUseCase {
+  constructor(private usersRepository: any){}
 
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exists.')
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest) {
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+  
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+  
+    const password_hash = await hash(password, 6)
+  
+    await this.usersRepository.create({
+      name, 
+      email, 
+      password_hash,
+    })
   }
 
-  const password_hash = await hash(password, 6)
-
-  const prismaUsersRepository  = new PrismaUsersRepository()
-
-  const user = await prismaUsersRepository.create({
-    name, 
-    email, 
-    password_hash,
-  })
 }
 
