ce8554514c18fc616c3449dd67081c89ce1138ad
Victor Sousa <victorbrito001@gmail.com>
2023-06-12 14:39:55 -0300

    Implementando JWT no Fastify

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,13 @@ import { ZodError } from 'zod'
 import { env } from './env'
 
 import { appRoutes } from './http/routes'
+import fastifyJwt from '@fastify/jwt'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
 
 app.register(appRoutes)
 
